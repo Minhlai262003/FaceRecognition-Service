d8d6649878270e30d250cdb19eea9178b37d6cba
hdchi <dinhchih5@gmail.com>
2025-07-02 11:13:14 +0700

    feat: init project

---
+++ src/test/java/com/enclave/FaceRecognition/FaceRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enclave.FaceRecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
